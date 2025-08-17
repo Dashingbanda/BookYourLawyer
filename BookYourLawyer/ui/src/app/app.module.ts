8f9f05aab1d5afddd6e2e97e659b50c9208ea2af
iamyschauhan <singhyuvraj647@gmail.com>
2025-08-18 02:36:28 +0530

    build and serve successfull

--- BookYourLawyer/ui/src/app/app.module.ts
+++ BookYourLawyer/ui/src/app/app.module.ts
@@ -9,13 +9,22 @@ import { AppComponent } from './app.component';
 
 // Angular Material Modules (add more as needed)
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import { BookingFormComponent } from './pages/booking-form/booking-form.component';
+import { RouterModule } from '@angular/router';
+import { LawyerDetailComponent } from './pages/lawyer-detail/lawyer-detail.component';
+import { LawyerListComponent } from './pages/lawyer-list/lawyer-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookingFormComponent,
+    LawyerDetailComponent,
+    LawyerListComponent
   ],
   imports: [
     BrowserModule,
@@ -23,9 +32,12 @@ import { MatCardModule } from '@angular/material/card';
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
+    RouterModule,
     AppRoutingModule,
 
     // Material modules
+    MatFormFieldModule,
+    MatInputModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
